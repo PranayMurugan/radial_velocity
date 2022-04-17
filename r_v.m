efd603c6248fc789c0440c38fa5ab8b49d14fdfd
Pranay Murugan <pranay.murugan01@gmail.com>
2022-04-17 11:08:18 +0530

    Update r_v.m

--- r_v.m
+++ r_v.m
@@ -5,13 +5,13 @@ a = 0.05;
 e = 0;
 g = [];
 
-%for k = 0:30:90
+for k = 0:30:90
     for j = 1:1:720
-        t = theta(j) + omega
-        r = (2 * pi * a * sin(i) * (cos(t) + e * cos(omega)))/(P * sqrt(1 - e^2));
+        t = theta(j) + k
+        r = (2 * pi * a * sin(i) * (cos(t) + e * cos(k)))/(P * sqrt(1 - e^2));
         g(j) = r;
     end
     hold on;
-    plot(g)
-%end
+    scatter(theta,g)
+end
 hold off;
