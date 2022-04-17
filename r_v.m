70f6190d33da527e593d35e4f6710d00e774c5f7
Pranay Murugan <pranay.murugan01@gmail.com>
2022-04-17 15:02:44 +0530

    Update r_v.m

--- r_v.m
+++ r_v.m
@@ -3,12 +3,10 @@ i = 60;
 P = 5;
 a = 0.05;
 e = 0;
-g = [];
 
-for k = 0:30:90
+for omega = 0:30:90
     for j = 0:1:720
-        t = theta(j+1) + k;
-        r = (2 * pi * a * sind(i) * (cosd(t) + e * cosd(k)))/(P * sqrt(1 - e^2));
+        r = (2 * pi * a * sind(i) * (cosd(theta(j+1) + omega) + e * cosd(omega)))/(P * sqrt(1 - e^2));
         g(j+1) = r;
     end
     hold on;
