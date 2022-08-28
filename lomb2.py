38de238162ebec811f9ac6b5d86c24ad06b18625
Pranay Murugan <pranay.murugan01@gmail.com>
2022-08-28 20:20:58 +0530

    Update lomb2.py

--- lomb2.py
+++ lomb2.py
@@ -1,3 +1,6 @@
+""" This program takes the data in the form of radial velocity and the time (in Julian days) at which the data was recorded,
+and applies Lomb-Scargle Periodogram, and finding the orbital time period, after finding the time period the radial velocity is folded to get the radial_velocity curve
+"""
 import matplotlib.pyplot as plt
 from astropy.timeseries import LombScargle
 import astropy.units as u
@@ -10,12 +13,16 @@ r = []
 dr = []
 new_t = []
 
+# Read the data file as a csv
+
 with open('/home/safesoccer/Projects/Planetary/RV_51Pegasi_Data.txt','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=' ')
     for row in plots:
         t.append(float(row[0]))
         r.append(float(row[1]))
         dr.append(float(row[2]))
+
+# simple scatter plot of the raw data
 
 plt.scatter(t,r, label='Radial velocity')
 plt.xlabel('Time period (in Julian days)')
@@ -24,12 +31,15 @@ plt.title('Scatter plot')
 plt.legend()
 plt.show()
 
+# Apply LombScargle Periodogram to find orbital time period
+
 frequency, power = LombScargle(t, r, dr).autopower()
 plt.plot(frequency, power)
 plt.show()
 
-peaks, height = find_peaks(power, height = 0.9)
-y = 1/(frequency[peaks])
+# Find the peak in the LombScargle probablity distribution
+
+peaks, height = find_peaks(power, height = 0.9)  # height = 0.9 refers the 90% probablity
 print(np.round(y,2))
 
 i = 0
@@ -37,9 +47,10 @@ i = 0
 for x in t:
     new_t.append(x % y)
 
+# Radial-velocity curve plotted
+
 plt.errorbar(new_t, r, yerr = dr, fmt="o")
 plt.xlabel('Time period')
 plt.ylabel('Radial velocity')
 plt.title('R_V curve')
 plt.show()
-
